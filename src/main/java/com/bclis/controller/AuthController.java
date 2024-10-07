675dd3b5d321262e4ac448e803155daeb4aa2dce
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2024-10-06 20:30:13 -0500

    docs: add swagger description for AuthController

--- src/main/java/com/bclis/controller/AuthController.java
+++ src/main/java/com/bclis/controller/AuthController.java
@@ -4,6 +4,9 @@ import com.bclis.dto.request.CreateUserDTO;
 import com.bclis.dto.request.LoginDTO;
 import com.bclis.dto.response.AuthResponseDTO;
 import com.bclis.service.UserDetailsServiceImpl;
+import com.bclis.utils.constans.ApiDescription;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,16 +14,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Authentication", description = ApiDescription.AUTH_CONTROLLER_DESCRIPTION)
 public class AuthController {
 
     private final UserDetailsServiceImpl userDetailsService;
 
     @PostMapping("/login")
+    @Operation(summary = "Login user", description = ApiDescription.LOGIN_DESCRIPTION)
     public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDTO loginDTO) {
         return ResponseEntity.ok(userDetailsService.login(loginDTO));
     }
 
     @PostMapping("/register")
+    @Operation(summary = "Register new user", description = ApiDescription.REGISTER_DESCRIPTION)
     public ResponseEntity<String> createUser(@RequestBody CreateUserDTO createUserDTO) {
         userDetailsService.createUser(createUserDTO);
         return ResponseEntity.ok("User created");
