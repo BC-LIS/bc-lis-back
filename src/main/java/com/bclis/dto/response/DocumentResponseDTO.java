91bad5e379bfc1705b566841483204d4d7e4d9b1
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-10-07 19:56:10 -0500

    fix: update dto for document response

--- src/main/java/com/bclis/dto/response/DocumentResponseDTO.java
+++ src/main/java/com/bclis/dto/response/DocumentResponseDTO.java
@@ -1,25 +1,21 @@
 package com.bclis.dto.response;
 
-import com.bclis.persistence.entity.DocumentEntity.DocumentState;
+import com.bclis.persistence.entity.DocumentEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DocumentResponseDTO {
-
     private Long id;
-    private Long userId;
-    private String userName;
     private String name;
-    private byte[] content;
+    private String description;
+    private String objectName;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-    private DocumentState state;
-    private Long typeId;
-    private String typeName;
+    private DocumentEntity.DocumentState state;
 }
+
