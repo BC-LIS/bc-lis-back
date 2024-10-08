1233e23ff85f580ea8cb9ac631dd25509ec4a760
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-10-07 19:51:16 -0500

    feat: add model mapper config file

--- src/main/java/com/bclis/configuration/ModelMapperConfig.java
+++ src/main/java/com/bclis/configuration/ModelMapperConfig.java
@@ -1,2 +1,23 @@
-package com.bclis.configuration;public class ModelMapperConfig {
+package com.bclis.configuration;
+
+import com.bclis.dto.request.DocumentCreateDTO;
+import com.bclis.persistence.entity.DocumentEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        // Configurar el mapeo específico para DocumentCreateDTO y DocumentEntity
+        modelMapper.createTypeMap(DocumentCreateDTO.class, DocumentEntity.class)
+                .addMappings(mapper -> mapper.map(DocumentCreateDTO::getState, DocumentEntity::setState));
+
+        return modelMapper;
+    }
 }
+
