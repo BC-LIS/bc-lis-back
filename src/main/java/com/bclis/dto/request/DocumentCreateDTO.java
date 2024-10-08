128c3d7e9f3fe19d4db9788f6f9d3d25e65b45d9
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-10-07 19:55:35 -0500

    fix: update dto for create document

--- src/main/java/com/bclis/dto/request/DocumentCreateDTO.java
+++ src/main/java/com/bclis/dto/request/DocumentCreateDTO.java
@@ -1,20 +1,18 @@
 package com.bclis.dto.request;
 
-import com.bclis.persistence.entity.DocumentEntity.DocumentState;
+import com.bclis.persistence.entity.DocumentEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class DocumentCreateDTO {
-
-    private Long userId;
     private String name;
-    private byte[] content;
-    private DocumentState state;
-    private Long typeId;
+    private String description;
+    private MultipartFile file;
+    private DocumentEntity.DocumentState state;
 }
+
