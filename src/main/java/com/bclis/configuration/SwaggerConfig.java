1fd524987317fc21d2609800e97668e7678dc2eb
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2024-10-13 22:35:11 -0500

    feat: add swagger config

--- src/main/java/com/bclis/configuration/SwaggerConfig.java
+++ src/main/java/com/bclis/configuration/SwaggerConfig.java
@@ -10,5 +10,38 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.http.HttpHeaders;
 
-
+@OpenAPIDefinition(
+        info = @Info(
+                title = "BCLIS API",
+                description = "Application that offers a set of services for the efficient management, creation, and organization of documents.",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Laboratorio Integrado de Sistemas - UdeA",
+                        url = "https://sistemas.udea.edu.co/",
+                        email = "laboratoriois@udea.edu.co"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "Development server",
+                        url = "http://localhost:8080/api/bclis"
+                ),
+                @Server(
+                        description = "Production server",
+                        url = "http://192.168.30.69:8080/api/bclis"
+                )
+        },
+        security = @SecurityRequirement(
+                name = "Security Token"
+        )
+)
+@SecurityScheme(
+        name = "Security Token",
+        description = "Access token for the API",
+        type = SecuritySchemeType.HTTP,
+        paramName = HttpHeaders.AUTHORIZATION,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfig { }
