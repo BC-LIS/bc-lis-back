c5135643b6b2832ab95a5ff923f0e867d71783ad
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2024-10-17 17:27:20 -0500

    feat: add service that creates a new category

--- src/main/java/com/bclis/service/CategoryService.java
+++ src/main/java/com/bclis/service/CategoryService.java
@@ -1,8 +1,11 @@
 package com.bclis.service;
 
+import com.bclis.dto.request.CreateCategoryDTO;
 import com.bclis.persistence.entity.CategoryEntity;
 import com.bclis.persistence.repository.CategoryRepository;
+import com.bclis.utils.exceptions.AlreadyExistsException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,9 +20,20 @@ public class CategoryService {
     public List<String> getAllCategories(){
         List<CategoryEntity> categories = categoryRepository.findAll();
         List<String> categoriesNames = new ArrayList<>();
-
         categories.forEach(c -> categoriesNames.add(c.getName()));
-
         return categoriesNames;
+    }
+
+    public void createCategory(CreateCategoryDTO categoryDTO){
+        String categoryName = categoryDTO.getCategoryName();
+        boolean existCategory = categoryRepository.existsByName(categoryName);
+
+        if(existCategory){
+            throw new AlreadyExistsException("409", HttpStatus.CONFLICT, "Category already exists");
+        }
+
+        CategoryEntity categoryEntity = new CategoryEntity();
+        categoryEntity.setName(categoryName);
+        categoryRepository.save(categoryEntity);
     }
 }
