913df7ee557fda31a882aa045d6c0bf442894cd9
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2024-10-17 14:48:32 -0500

    feat: add endpoint that returns all categories

--- src/main/java/com/bclis/controller/CategoryController.java
+++ src/main/java/com/bclis/controller/CategoryController.java
@@ -1,18 +1,29 @@
 package com.bclis.controller;
 
+import com.bclis.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/categories")
 @RequiredArgsConstructor
 public class CategoryController {
 
+    private final CategoryService categoryService;
+
     @GetMapping("/test")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("This a test for category controller");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<String>> getAllCategories() {
+        List<String> categories = categoryService.getAllCategories();
+        return ResponseEntity.ok(categories);
     }
 }
