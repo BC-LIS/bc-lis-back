f435dc70c6dbc1186b5f24e638366a2b872828d3
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2024-10-17 22:14:14 -0500

    feat: update class structure

--- src/main/java/com/bclis/utils/exceptions/AlreadyExistsException.java
+++ src/main/java/com/bclis/utils/exceptions/AlreadyExistsException.java
@@ -3,18 +3,13 @@ package com.bclis.utils.exceptions;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 @Setter
 @Getter
 public class AlreadyExistsException extends RuntimeException {
-    private final String code;
-    private final HttpStatus status;
 
-    public AlreadyExistsException(String code, HttpStatus status, String message) {
+    public AlreadyExistsException(String message) {
         super(message);
-        this.status = status;
-        this.code = code;
     }
 }
