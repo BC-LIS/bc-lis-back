a340c28b6c03946464c4cfe17e09029cc48d8943
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2024-10-20 15:58:23 -0500

    feat: add specification class that filters a document by 'containsAttribute' and 'hasAttribute'

--- src/main/java/com/bclis/persistence/repository/DocumentRepository.java
+++ src/main/java/com/bclis/persistence/repository/DocumentRepository.java
@@ -2,9 +2,10 @@ package com.bclis.persistence.repository;
 
 import com.bclis.persistence.entity.DocumentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DocumentRepository extends JpaRepository<DocumentEntity, Long> {
+public interface DocumentRepository extends JpaRepository<DocumentEntity, Long>, JpaSpecificationExecutor<DocumentEntity> {
     boolean existsByTypeId(Long id);
 }
---
+++ src/main/java/com/bclis/persistence/specification/DocumentSpecification.java
@@ -0,0 +1,16 @@
+package com.bclis.persistence.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DocumentSpecification {
+
+    public <T> Specification<T> hasAttribute(String attributeName, Object value) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get(attributeName), value);
+    }
+
+    public <T> Specification<T> containsAttribute(String attributeName, Object value) {
+        return (root, query, criteriaBuilder) -> criteriaBuilder.like(root.get(attributeName), "%"+value+"%");
+    }
+}
