1368418ee785ad8d44c352cbb7184b2a4677dbbb
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-10-26 14:05:19 -0500

    feat: add method to find documents in repository

--- src/main/java/com/bclis/persistence/repository/DocumentCategoryRepository.java
+++ src/main/java/com/bclis/persistence/repository/DocumentCategoryRepository.java
@@ -1,10 +1,14 @@
 package com.bclis.persistence.repository;
 
 import com.bclis.persistence.entity.DocumentCategoryEntity;
+import com.bclis.persistence.entity.DocumentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DocumentCategoryRepository extends JpaRepository<DocumentCategoryEntity, Long> {
     boolean existsByCategoryId(Long categoryId);
+    List<DocumentCategoryEntity> findByDocument(DocumentEntity document);
 }
