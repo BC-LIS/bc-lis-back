aa110ad541f9535a679e9327237857d72c847c6a
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-10-26 09:35:01 -0500

    feat: add user dto response

---
+++ src/main/java/com/bclis/dto/response/UserResponseDTO.java
@@ -0,0 +1,12 @@
+package com.bclis.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDTO {
+    private String username; // Solo el username
+}
