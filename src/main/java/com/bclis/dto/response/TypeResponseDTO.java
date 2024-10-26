4efe192c5c4dfdcf33ab995099f21263eae0cefd
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-10-26 09:33:59 -0500

    feat: add type dto response

---
+++ src/main/java/com/bclis/dto/response/TypeResponseDTO.java
@@ -0,0 +1,12 @@
+package com.bclis.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TypeResponseDTO {
+    private String name; // Solo el nombre
+}
