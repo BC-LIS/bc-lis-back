2e88fd39365a6694550bfd0b818514f2f7afe9a5
Juan Felipe Escobar <juan.escobar15@udea.edu.co>
2024-11-04 21:49:27 -0500

    feat: add DTO to return the information of a comment

---
+++ src/main/java/com/bclis/dto/response/CommentResponseDTO.java
@@ -0,0 +1,20 @@
+package com.bclis.dto.response;
+
+import com.bclis.persistence.entity.CommentEntity;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentResponseDTO {
+    private int id;
+    private String content;
+    private LocalDateTime createdAt;
+    private CommentEntity.CommentState commentState;
+    private UserResponseDTO user;
+    private Long documentId;
+}
