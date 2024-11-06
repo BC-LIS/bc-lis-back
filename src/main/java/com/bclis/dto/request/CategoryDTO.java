50bd6473333b12a90e36650330fe5b3c67ff2757
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-11-05 23:54:56 -0500

    docs: add documentation for category dto

--- src/main/java/com/bclis/dto/request/CategoryDTO.java
+++ src/main/java/com/bclis/dto/request/CategoryDTO.java
@@ -1,5 +1,6 @@
 package com.bclis.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Setter
@@ -8,5 +9,11 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class CategoryDTO {
+    @Schema(description = "Name of the document category",
+            example = "Finance",
+            type = "string",
+            required = true,
+            minLength = 3,
+            maxLength = 50)
     private String categoryName;
 }
