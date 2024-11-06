85c7347000faae18c37544106b3c6b9693f68f65
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-11-05 23:55:39 -0500

    docs: add documentation for create document dto

--- src/main/java/com/bclis/dto/request/DocumentCreateDTO.java
+++ src/main/java/com/bclis/dto/request/DocumentCreateDTO.java
@@ -1,6 +1,8 @@
 package com.bclis.dto.request;
 
 import com.bclis.persistence.entity.DocumentEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,12 +14,59 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DocumentCreateDTO {
+    @Schema(description = "Name of the document",
+            example = "Project Proposal",
+            type = "string",
+            required = true,
+            minLength = 3,
+            maxLength = 100)
     private String name;
+
+
+    @Schema(description = "Description of the document",
+            example = "Detailed project proposal document for Q1 2024",
+            type = "string",
+            maxLength = 500)
     private String description;
+
+
+    @Schema(description = "File to be uploaded, representing the document content",
+            type = "string",
+            format = "binary",
+            required = true)
     private MultipartFile file;
+
+
+    @Schema(description = "Current state of the document",
+            example = "DRAFT",
+            type = "string",
+            allowableValues = {"DRAFT", "PUBLISHED", "ARCHIVED"},
+            required = true)
     private DocumentEntity.DocumentState state;
+
+
+    @Schema(description = "Type of the document (for example, Invoice, Report, etc.)",
+            example = "Report",
+            type = "string",
+            required = true,
+            minLength = 3,
+            maxLength = 50)
     private String typeName;
+
+
+    @Schema(description = "Username of the creator of the document",
+            example = "jdoe",
+            type = "string",
+            required = true)
     private String username;
+
+    /**
+     * List of categories associated with the document, allowing categorization and tagging.
+     */
+    @Schema(description = "Categories associated with the document, must contain at least one category",
+            example = "[\"Ansible\", \"Docker\", \"Projects\"]",
+            type = "array")
+    @Size(min = 1, message = "At least one category is required")
     private List<String> categories;
 }
 
