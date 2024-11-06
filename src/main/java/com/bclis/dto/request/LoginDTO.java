26983beb763dbc2da1e9957abac43a2b0223f646
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-11-05 23:56:06 -0500

    docs: add documentation for login dto

--- src/main/java/com/bclis/dto/request/LoginDTO.java
+++ src/main/java/com/bclis/dto/request/LoginDTO.java
@@ -1,5 +1,7 @@
 package com.bclis.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,6 +10,18 @@ import lombok.Setter;
 @Getter
 @Builder
 public class LoginDTO {
+    @Schema(description = "Username of the user attempting to log in",
+            example = "jdoe",
+            type = "string",
+            required = true)
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @Schema(description = "Password of the user attempting to log in",
+            example = "securePassword123",
+            type = "string",
+            required = true,
+            format = "password")
+    @NotBlank(message = "Password is required")
     private String password;
 }
