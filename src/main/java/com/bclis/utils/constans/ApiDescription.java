eb843e2d6b63c40ca0c334c1a77f166f521f920b
ValenMR <valentina.munozr1@udea.edu.co>
2024-11-16 17:41:37 -0500

    docs: swagger documentation of category endpoints

--- src/main/java/com/bclis/utils/constans/ApiDescription.java
+++ src/main/java/com/bclis/utils/constans/ApiDescription.java
@@ -10,6 +10,20 @@ public class ApiDescription {
     public static final String LOGIN_DESCRIPTION = "Endpoint that allows users to log in using their username and password.";
     public static final String REGISTER_DESCRIPTION = "Endpoint that allows the creation of new users";
 
+    //CategoryController
+    public static final String CATEGORY_CONTROLLER_DESCRIPTION = "This controller manages the categories that can exist for each document.";
+    public static final String GET_CATEGORY_DESCRIPTION = "Endpoint that allows retrieving all available categories. \n"
+            + "\n"
+            +"It can be accessed by all existing roles.";
+
+    public static final String CREATE_CATEGORY_DESCRIPTION = "Endpoint that allows create a category and you must indicate the name of the category in the body of the request. \n" +
+            "\n"
+            +"It can be accessed by all existing roles.";
+
+    public static final String DELETE_CATEGORY_DESCRIPTION = "Endpoint that allows deleting a category by specifying its name \n" +
+            "\n"
+            +"It can be accessed by all existing roles.";
+
     // DocumentController
     public static final String DOCUMENT_CONTROLLER_DESCRIPTION = "This controller handles operations related to document management.";
     public static final String CREATE_DOCUMENT_DESCRIPTION = "Endpoint that allows the creation of new documents.";
