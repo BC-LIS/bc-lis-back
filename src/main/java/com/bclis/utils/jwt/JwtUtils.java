133c4df59a24f46c943f01796a02d44b6470638c
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2025-06-09 23:31:05 -0500

    feat: enable setter access for JwtUtils tests

--- src/main/java/com/bclis/utils/jwt/JwtUtils.java
+++ src/main/java/com/bclis/utils/jwt/JwtUtils.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -20,6 +21,7 @@ import java.util.stream.Collectors;
 
 @Component
 @Slf4j
+@Setter
 public class JwtUtils {
 
     @Value("${jwt.secret.key}")
